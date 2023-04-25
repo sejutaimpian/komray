2dbadbd9eb789f464392e85e1d3a55e6996939a4
sejutaimpian <derissulistina@gmail.com>
2023-04-25 16:33:46 +0700

    feat: navigasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '360px'
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
