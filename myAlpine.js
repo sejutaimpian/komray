0adc2b2c45fcf5a633c983a44904864c2e21370f
sejutaimpian <derissulistina@gmail.com>
2023-05-27 10:40:00 +0700

    add: profile section

---
+++ myAlpine.js
@@ -0,0 +1,11 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('tabs', () => ({
+        activeTab: 1,
+        activeClass: 'bg-blue-800 border-2 border-white ring-2 ring-blue-800 text-white',
+        inActiveClass: 'bg-transparent border-2 border-gray-500 text-gray-500',
+        setActive(tab){
+            this.activeTab = tab;
+            location = '#tab-konten';
+        }
+    }))
+})
