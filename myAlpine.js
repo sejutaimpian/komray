1a55bc822b84930d731ae64e62d7272352d02f9c
sejutaimpian <derissulistina@gmail.com>
2023-06-03 10:34:36 +0700

    add konten kepengurusan

--- myAlpine.js
+++ myAlpine.js
@@ -1,11 +1,12 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('tabs', () => ({
-        activeTab: 1,
-        activeClass: 'bg-blue-800 border-2 border-white ring-2 ring-blue-800 text-white',
-        inActiveClass: 'bg-transparent border-2 border-gray-500 text-gray-500',
-        setActive(tab){
-            this.activeTab = tab;
-            location = '#tab-konten';
-        }
-    }))
-})
+document.addEventListener("alpine:init", () => {
+  Alpine.data("tabs", () => ({
+    activeTab: 3,
+    activeClass:
+      "bg-blue-800 border-2 border-white ring-2 ring-blue-800 text-white",
+    inActiveClass: "bg-transparent border-2 border-gray-500 text-gray-500",
+    setActive(tab) {
+      this.activeTab = tab;
+      location = "#tab-konten";
+    },
+  }));
+});
