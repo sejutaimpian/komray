06e21c7f68968c06fedd4f9020492bf384446435
sejutaimpian <derissulistina@gmail.com>
2023-06-05 21:07:00 +0700

    add prestasi

--- myAlpine.js
+++ myAlpine.js
@@ -3,7 +3,7 @@ document.addEventListener("alpine:init", () => {
     activeTab: 1,
     activeClass:
       "bg-blue-800 border-2 border-white ring-2 ring-blue-800 text-white",
-    inActiveClass: "bg-transparent border-2 border-gray-500 text-gray-500",
+    inActiveClass: "bg-transparent border-2 border-gray-500 text-gray-500 hover:bg-white hover:text-black",
     setActive(tab) {
       this.activeTab = tab;
       location = "#tab-konten";
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        kocok: 'kocok 1s ease-in-out',
+      },
+      keyframes: {
+        kocok: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(7rem)' },
+        }
+      },
       screens: {
         'xs': '360px',
         'sm': '576px',
