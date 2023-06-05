5fc97660694271b06183386f04006bc65a1fd8b9
sejutaimpian <derissulistina@gmail.com>
2023-06-05 07:18:46 +0700

    add hashtag judul sub profile

--- myAlpine.js
+++ myAlpine.js
@@ -1,6 +1,6 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("tabs", () => ({
-    activeTab: 3,
+    activeTab: 1,
     activeClass:
       "bg-blue-800 border-2 border-white ring-2 ring-blue-800 text-white",
     inActiveClass: "bg-transparent border-2 border-gray-500 text-gray-500",
